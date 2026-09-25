eb5ccaa99bfb5c3fbec3b3e195a837e6379144d4
pos <pgousdal@users.noreply.github.com>
2026-09-25 21:00:10 +0200

    M0.1: add examples/secret_word.py

---
+++ examples/secret_word.py
@@ -0,0 +1,8 @@
+"""EduPython Kids: a first if/else program."""
+
+secret = input("Hva er det hemmelige ordet? ")
+
+if secret.lower() == "python":
+    print("Døren åpner seg!")
+else:
+    print("Døren er fortsatt låst.")
