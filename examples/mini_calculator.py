6b84910036a8685cbb2cf990ae3e7612a4e57ac7
pos <pgousdal@users.noreply.github.com>
2026-09-25 21:00:06 +0200

    M0.1: add examples/mini_calculator.py

---
+++ examples/mini_calculator.py
@@ -0,0 +1,8 @@
+"""EduPython Kids: a tiny two-number calculator."""
+
+a = int(input("Første tall: "))
+b = int(input("Andre tall: "))
+
+print("Sum:", a + b)
+print("Differanse:", a - b)
+print("Produkt:", a * b)
